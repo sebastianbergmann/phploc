c069bf44bc7e22e776e0db00e115d1bcbbdcfd90
jeroendedauw <jeroendedauw@gmail.com>
2014-07-27 13:58:23 +0200

    Improve doc block in Command

--- src/CLI/Command.php
+++ src/CLI/Command.php
@@ -268,9 +268,9 @@ namespace SebastianBergmann\PHPLOC\CLI
         }
 
         /**
-         * @param  Symfony\Component\Console\Input\InputOption $input
-         * @param  string                                      $option
-         * @return array
+         * @param  InputInterface $input
+         * @param  string         $option
+         * @return string[]
          */
         private function handleCSVOption(InputInterface $input, $option)
         {
