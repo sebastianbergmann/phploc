d6b05c9072ed7ec3e365722d129f220c24aa90f1
Sebastian Bergmann <sb@sebastian-bergmann.de>
2015-05-26 16:03:47 +0200

    Prepare release

--- src/CLI/Application.php
+++ src/CLI/Application.php
@@ -29,7 +29,7 @@ class Application extends AbstractApplication
 {
     public function __construct()
     {
-        $version = new Version('2.1.1', dirname(dirname(__DIR__)));
+        $version = new Version('2.1.2', dirname(dirname(__DIR__)));
         parent::__construct('phploc', $version->getVersion());
     }
 
