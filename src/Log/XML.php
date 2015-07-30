a2ac0d706d70797d9d9fe52ee0678f0f9c104ab7
Shivanshu Agrawal <shivanshuag@gmail.com>
2015-07-30 13:03:27 +0530

    Provide human readable names of metrics in xml report

--- src/Log/XML.php
+++ src/Log/XML.php
@@ -18,6 +18,66 @@ namespace SebastianBergmann\PHPLOC\Log;
 class XML
 {
     /**
+     * Mapping between internal and human-readable metric names
+     *
+     * @var array
+     */
+    private $colmap = array(
+        'directories'                 => 'Directories',
+        'files'                       => 'Files',
+        'loc'                         => 'Lines of Code (LOC)',
+        'ccnByLloc'                   => 'Cyclomatic Complexity / Lines of Code',
+        'cloc'                        => 'Comment Lines of Code (CLOC)',
+        'ncloc'                       => 'Non-Comment Lines of Code (NCLOC)',
+        'lloc'                        => 'Logical Lines of Code (LLOC)',
+        'llocGlobal'                  => 'LLOC outside functions or classes',
+        'namespaces'                  => 'Namespaces',
+        'interfaces'                  => 'Interfaces',
+        'traits'                      => 'Traits',
+        'classes'                     => 'Classes',
+        'abstractClasses'             => 'Abstract Classes',
+        'concreteClasses'             => 'Concrete Classes',
+        'llocClasses'                 => 'Classes Length (LLOC)',
+        'methods'                     => 'Methods',
+        'nonStaticMethods'            => 'Non-Static Methods',
+        'staticMethods'               => 'Static Methods',
+        'publicMethods'               => 'Public Methods',
+        'nonPublicMethods'            => 'Non-Public Methods',
+        'classLlocAvg'                => 'Average Class Length (LLOC)',
+        'classLlocMin'                => 'Minimum Class Length (LLOC)',
+        'classLlocMax'                => 'Maximum Class Length (LLOC)',
+        'classCcnAvg'                 => 'Cyclomatic Complexity / Number of Classes',
+        'classCcnMin'                 => 'Minimum Class Complexity',
+        'classCcnMax'                 => 'Maximum Class Complexity',
+        'methodLlocAvg'               => 'Average Method Length (LLOC)',
+        'methodLlocMin'               => 'Minimal Method Length (LLOC)',
+        'methodLlocMax'               => 'Maximal Method Length (LLOC)',
+        'methodCcnAvg'                => 'Cyclomatic Complexity / Number of Methods',
+        'methodCcnMin'                => 'Minimum Method Complexity',
+        'methodCcnMax'                => 'Maximum Method Complexity',
+        'functions'                   => 'Functions',
+        'namedFunctions'              => 'Named Functions',
+        'anonymousFunctions'          => 'Anonymous Functions',
+        'llocFunctions'               => 'Functions Length (LLOC)',
+        'llocByNof'                   => 'Average Function Length (LLOC)',
+        'constants'                   => 'Constants',
+        'globalConstants'             => 'Global Constants',
+        'classConstants'              => 'Class Constants',
+        'attributeAccesses'           => 'Attribute Accesses',
+        'instanceAttributeAccesses'   => 'Non-Static Attribute Accesses',
+        'staticAttributeAccesses'     => 'Static Attribute Accesses',
+        'methodCalls'                 => 'Method Calls',
+        'instanceMethodCalls'         => 'Non-Static Method Calls',
+        'staticMethodCalls'           => 'Static Method Calls',
+        'globalAccesses'              => 'Global Accesses',
+        'globalVariableAccesses'      => 'Global Variable Accesses',
+        'superGlobalVariableAccesses' => 'Super-Global Variable Accesses',
+        'globalConstantAccesses'      => 'Global Constant Accesses',
+        'testClasses'                 => 'Test Classes',
+        'testMethods'                 => 'Test Methods'
+    );
+
+    /**
      * Prints a result set.
      *
      * @param string $filename
@@ -45,9 +105,15 @@ class XML
         unset($count['files']);
 
         foreach ($count as $k => $v) {
-            $root->appendChild(
+            $metric = $root->appendChild(
                 $document->createElement($k, $v)
             );
+            if (isset($this->colmap[$k])) {
+                $metric->setAttribute("name", $this->colmap[$k]);
+            }
+            else {
+                $metric->setAttribute("name", '');
+            }
         }
 
         file_put_contents($filename, $document->saveXML());
