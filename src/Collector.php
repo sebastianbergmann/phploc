a4c3c621762523ae96d49f41d315fc5f852d8458
Kuba Werłos <werlos@gmail.com>
2016-12-12 17:42:58 +0100

    Size block

--- src/Collector.php
+++ src/Collector.php
@@ -17,16 +17,62 @@ class Collector
         $this->addUnique('directories', dirname($filename));
     }
 
+    public function incrementLines($number)
+    {
+        $this->increment('lines', $number);
+    }
+
+    public function incrementCommentLines($number)
+    {
+        $this->increment('comment lines', $number);
+    }
+
+    public function incrementLogicalLines()
+    {
+        $this->increment('logical lines');
+    }
+
+    public function addClassLines($number)
+    {
+        $this->addToArray('class lines', $number);
+    }
+
+    public function addMethodLines($number)
+    {
+        $this->addToArray('method lines', $number);
+    }
+
+    public function incrementFunctionLines()
+    {
+        $this->increment('function lines');
+    }
+
+    public function incrementNamedFunctions()
+    {
+        $this->increment('named functions');
+    }
+
+    public function incrementAnonymousFunctions()
+    {
+        $this->increment('anonymous functions');
+    }
+
     private function addUnique($key, $name)
     {
         $this->check($key, []);
         $this->counts[$key][$name] = true;
     }
 
-    private function increment($key)
+    private function addToArray($key, $value)
+    {
+        $this->check($key, []);
+        $this->counts[$key][] = $value;
+    }
+
+    private function increment($key, $number = 1)
     {
         $this->check($key, 0);
-        $this->counts[$key]++;
+        $this->counts[$key] += $number;
     }
 
     private function check($key, $default)
