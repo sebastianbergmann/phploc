6eb51bd055fa9aa025f80fb23b36cc874762c9ba
Sebastian Bergmann <sb@sebastian-bergmann.de>
2017-02-05 09:01:42 +0100

    Bump

--- src/CLI/Application.php
+++ src/CLI/Application.php
@@ -25,7 +25,7 @@ class Application extends AbstractApplication
 {
     public function __construct()
     {
-        $version = new Version('3.0.1', dirname(dirname(__DIR__)));
+        $version = new Version('3.1', dirname(dirname(__DIR__)));
         parent::__construct('phploc', $version->getVersion());
     }
 
