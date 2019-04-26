14f1a4c1b2cadf3dc583dea93e558ad11606bd0e
Kuba Werłos <werlos@gmail.com>
2019-04-26 21:24:18 +0200

    Tests

---
+++ tests/_files/with_declare.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+function foo()
+{
+    return 42;
+}
---
+++ tests/_files/with_namespace.php
@@ -0,0 +1,8 @@
+<?php
+
+use TheHitchhikersGuideToTheGalaxy\DeepThought;
+
+function foo()
+{
+    return DeepThought::THE_ANSWER_TO_THE_ULTIMATE_QUESTION_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING;
+}
